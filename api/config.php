6a3996960daa0b8352922d2342da0f439b5acd4b
mirkb06 <mirkashem787@gmail.com>
2026-09-08 16:16:39 +0530

    config change to server

--- api/config.php
+++ api/config.php
@@ -4,15 +4,15 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 
-// $host = getenv('DB_HOST') ?: "localhost";
-// $username = getenv('DB_USER') ?: "prismabit.co.in";
-// $password = getenv('DB_PASS') ?: "P@55w0rd";
-// $database = getenv('DB_NAME') ?: "crm_prismabit";
-
 $host = getenv('DB_HOST') ?: "localhost";
-$username = getenv('DB_USER') ?: "root";
-$password = getenv('DB_PASS') ?: "";
+$username = getenv('DB_USER') ?: "prismabit.co.in";
+$password = getenv('DB_PASS') ?: "P@55w0rd";
 $database = getenv('DB_NAME') ?: "crm_prismabit";
+
+// $host = getenv('DB_HOST') ?: "localhost";
+// $username = getenv('DB_USER') ?: "root";
+// $password = getenv('DB_PASS') ?: "";
+// $database = getenv('DB_NAME') ?: "crm_prismabit";
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $database);
